d9d4466814ab3d97c4dd9d7103a39a91cf454bd2
tbrunz <51008435+tbrunz@users.noreply.github.com>
2020-04-05 21:09:03 -0700

    Design & code a new command line parser

--- src/install-pharo-launcher.sh
+++ src/install-pharo-launcher.sh
@@ -17,12 +17,12 @@ INSTALLER_DIR_NAME_GREP="pharolauncher"
 APPLICATION_SCRIPT_NAME="pharo-launcher"
 
 INSTALLER_SEARCH_PATHS=(
-    '.'
-    '${HOME}/Downloads'
-    '${HOME}'
+    "."
+    "${HOME}/Downloads"
+    "${HOME}"
 )
 
-DEFAULT_DESTINATION='${HOME}/Pharo'
+DEFAULT_DESTINATION="${HOME}/Pharo"
 
 USAGE_INSTALLER="[[-i] /inst/folder[/<inst.zip>]]"
 USAGE_DESTINATION="[[-d] /dest/folder]"
@@ -33,8 +33,7 @@ USAGE_PROMPT="usage: ${THIS_APP} ${USAGE_INSTALLER} ${USAGE_DESTINATION}"
 ###############################################################################
 #
 Display_Usage () {
-    echo "
-${USAGE_PROMPT}"
+    echo "${USAGE_PROMPT}"
 }
 
 
@@ -89,15 +88,7 @@ ${USAGE_PROMPT}
 # Function return codes
 #
 SUCCESS=0
-IGNORED=1
-NOT_APP=2
-IS_APP=3
-NO_FILES=4
-NO_SCRIPTS=5
-HAS_DIRS=6
-NO_DIRS=7
-VM_DIR=8
-CANT_WRITE=9
+BAD_SWITCH=1
 
 
 ###############################################################################
@@ -146,6 +137,42 @@ Display_Error () {
 
     # If $2 is defined, then quit the script, using $2 as the exit code.
     die $(( ${EXIT_SIGNAL} ))
+}
+
+
+###############################################################################
+#
+# Notify the user that this command line switch must be the only argument.
+#
+Error_Invalid_Switch () {
+    Display_Error "Invalid switch '${1}': must appear by itself" $BAD_SWITCH
+}
+
+
+###############################################################################
+#
+# Notify the user that this command line switch was found more than once.
+#
+Error_Duplicate_Switch () {
+    Display_Error "Duplicate command line switch: '${1}'" $BAD_SWITCH
+}
+
+
+###############################################################################
+#
+# Notify the user that the command line switch is not recognized.
+#
+Error_Unknown_Switch () {
+    Display_Error "Unknown command line switch, '${1}' " $BAD_SWITCH
+}
+
+
+###############################################################################
+#
+# Notify the user that this command line switch requires an argument.
+#
+Error_Missing_Argument () {
+    Display_Error "Missing argument for switch '${1}'" $BAD_SWITCH
 }
 
 
@@ -308,22 +335,149 @@ Ensure_is_Not_a_VM_Directory () {
 }
 
 
-
 ###############################################################################
 ###############################################################################
 #
+Install_Pharo_Launcher () {
+    echo "installing to '${DESTINATION_PATH}' "
+    echo "searching for installers in "
+    for PATH in "${INSTALLER_SEARCH_PATHS[@]}"; do
+        echo "    '${PATH}' "
+    done
+}
 
 
+###############################################################################
+#
+Process_Switch () {
+    local ARG1=${1}
+    local ARG2=${2}
+
+    # Reduce the switch to the lower-case version of its first character.
+    SWITCH=${ARG1:0:1}
+    SWITCH=${SWITCH,,}
+
+    # If ARG2 is also a switch, ignore it & return the ARG1 switch.
+    [[ "${ARG2:0:1}" == "-" ]] && return
+
+    # If the switch is a type that does not have an argument, return it.
+    [[ "${SWITCH}" == 'u' ]] && return
+    [[ "${SWITCH}" == 'h' ]] && return
+
+    # Then this is a switch that must have an argument, so consume ARG2.
+    PARAMETER=${ARG2}
+    DOUBLE_SHIFT=true
+}
+
+
+###############################################################################
+#
+Parse_Parameters () {
+    local ARG1=${1}
+    local ARG2=${2}
+
+    # We need to set three globals as a result of parsing ARG1/ARG2.
+    SWITCH=""
+    PARAMETER=""
+    DOUBLE_SHIFT=""
+
+    # If this is not a switch, return it as a parameter & ignore ARG2.
+    if [[ "${ARG1:0:1}" != "-" ]]; then
+        PARAMETER=${ARG1}
+        return
+    fi
+
+    # Remove the leading '-'.
+    ARG1=${ARG1#?}
+
+    # If the parameter starts with only one '-', it's a standalone switch.
+    if [[ "${ARG1:0:1}"  != '-' ]]; then
+        Process_Switch "${ARG1}" "${ARG2}"
+        return
+    fi
+
+    # Remove the second '-'.
+    ARG1=${ARG1#?}
+
+    # Now start looking for an '=' sign.
+    for (( THIS_CHAR=0; THIS_CHAR<${#ARG1}; THIS_CHAR++ )); do
+
+        if [[ "${ARG1:${THIS_CHAR}:1}" == '=' ]]; then
+            # Split the ARG1 string into two & ignore ARG2.
+            THIS_CHAR=$(( ++THIS_CHAR ))
+            ARG2=${ARG1:${THIS_CHAR}}
+
+            # Process the switch part & the parameter part separately.
+            Process_Switch "${ARG1}" "${ARG2}"
+
+            # We must NOT shift twice, since we split ARG1 in this case.
+            DOUBLE_SHIFT=""
+            return
+        fi
+    done
+
+    # If we reach this point, then there is no '=' in ARG1.
+    # In this case, treat it as though it were a standalone switch.
+    Process_Switch "${ARG1}" "${ARG2}"
+}
 
 
 ###############################################################################
 #
 Parse_Command_Line () {
-    # [-h|-u] [[-i] /inst/folder[/<inst.zip>]] [[-d] /dest/folder]
+    USAGE_SWITCH=
+    HELP_SWITCH=
+    INSTALLER_PATH=
+    DESTINATION_PATH=
+    DUPLICATE_SWITCH=
 
-    # Get the command line switches, as either '-s' or '--switch'.
-    COMMAND_LINE_SWITCH=${1,,}
-    COMMAND_LINE_SWITCH=${COMMAND_LINE_SWITCH##*-}
+    # One by one, step through the list of command line parameters.
+    while [[ -n "${1}" ]]; do
+
+        # Parse this parameter + the following, to allow for '-s <arg>'.
+        Parse_Parameters "${1}" "${2}"
+
+        case ${SWITCH} in
+        'u' )
+            if [[ -z "${USAGE_SWITCH}" ]]; then
+                USAGE_SWITCH="-u"
+            else
+                [[ -z "${DUPLICATE_SWITCH}" ]] && DUPLICATE_SWITCH=${SWITCH}
+            fi
+            ;;
+        'h' )
+            if [[ -z "${HELP_SWITCH}" ]]; then
+                HELP_SWITCH="-h"
+            else
+                [[ -z "${DUPLICATE_SWITCH}" ]] && DUPLICATE_SWITCH=${SWITCH}
+            fi
+            ;;
+        'i' )
+            [[ -n "${PARAMETER}" ]] || Error_Missing_Argument "-${SWITCH}"
+
+            if [[ -z "${INSTALLER_PATH}" ]]; then
+                INSTALLER_PATH=${PARAMETER}
+            else
+                [[ -z "${DUPLICATE_SWITCH}" ]] && DUPLICATE_SWITCH=${SWITCH}
+            fi
+            ;;
+        'd' )
+            [[ -n "${PARAMETER}" ]] || Error_Missing_Argument "-${SWITCH}"
+
+            if [[ -z "${DESTINATION_PATH}" ]]; then
+                DESTINATION_PATH=${PARAMETER}
+            else
+                [[ -z "${DUPLICATE_SWITCH}" ]] && DUPLICATE_SWITCH=${SWITCH}
+            fi
+            ;;
+        * )
+            Error_Unknown_Switch "${SWITCH}"
+            ;;
+        esac
+
+        shift
+        [[ -n "${DOUBLE_SHIFT}" ]] && shift
+    done
 }
 
 
@@ -333,16 +487,34 @@ Main () {
     # Read & parse the command line parameters.
     Parse_Command_Line "${@}"
 
-    case ${COMMAND_LINE_SWITCH} in
-    'u' )
+    # The 'usage' switch must appear by itself.
+    if [[ -n "${USAGE_SWITCH}" ]]; then
+        [[ -n "${HELP_SWITCH}${INSTALLER_PATH}${DESTINATION_PATH}" ]] && \
+            Error_Invalid_Switch "${USAGE_SWITCH}"
         Display_Usage && die
-        ;;
-    'h' )
+    fi
+
+    # The 'help' switch must appear by itself.
+    if [[ -n "${HELP_SWITCH}" ]]; then
+        [[ -n "${USAGE_SWITCH}${INSTALLER_PATH}${DESTINATION_PATH}" ]] && \
+            Error_Invalid_Switch "${HELP_SWITCH}"
         Display_Help && die
-        ;;
-    * )
-        echo "Doing this-and-that..."
-    esac
+    fi
+
+    # There must be no duplicate switches.
+    if [[ -n "${DUPLICATE_SWITCH}" ]]; then
+        Error_Duplicate_Switch "-${DUPLICATE_SWITCH}"
+    fi
+
+    # If the destination path is missing, default it.
+    [[ -n "${DESTINATION_PATH}" ]] || DESTINATION_PATH=${DEFAULT_DESTINATION}
+
+    # If the installer path is provided, substitute it for the search list.
+    [[ -n "${INSTALLER_PATH}" ]] && \
+        INSTALLER_SEARCH_PATHS=( "${INSTALLER_PATH}" )
+
+    # At this point, we have a valid command line, so install.
+    Install_Pharo_Launcher
 }
 
 Main "$@"
